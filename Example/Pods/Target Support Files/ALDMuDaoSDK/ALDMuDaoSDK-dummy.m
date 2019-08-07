0533b9b248944e1e2f7de80c93241e1d8fd1d09e
Jun <frank204866@gmail.com>
2019-08-07 14:21:40 +0800

    first commit

---
+++ Example/Pods/Target Support Files/ALDMuDaoSDK/ALDMuDaoSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ALDMuDaoSDK : NSObject
+@end
+@implementation PodsDummy_ALDMuDaoSDK
+@end
---
+++ Example/Pods/Target Support Files/Pods-ALDMuDaoSDK_Tests/Pods-ALDMuDaoSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ALDMuDaoSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ALDMuDaoSDK_Tests
+@end
